6644c9e375c9834e418966dbe756a5479d7c36cf
alexandrerosa-dev <asr.alexandrerosa@gmail.com>
2023-06-13 18:40:31 -0300

    GUI part 2

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    // Voltar ao menu principal
+    public void Quit()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Pular o turno
+    //TODO Finish this code
+    public void SkipTurn()
+    {
+        Debug.Log("Skip turn");
+    }
+}
---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    // Código para voltar ao menu principal
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
