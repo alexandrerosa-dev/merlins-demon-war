df7487d1fe27ec4b1a963ab930aae824012a157f
alexandrerosa-dev <asr.alexandrerosa@gmail.com>
2023-06-14 19:20:24 -0300

    Creating Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public Image playerImage       = null;
+    public Image mirrorImage       = null;
+    public Image healthNumberImage = null;
+    public Image glowImage         = null;
+
+    public int health = 5;
+    public int mana = 1;
+
+    public bool isPlayer;
+    public bool isFire; // Whether an enemy is a fire monster or not / casa o inimigo seja um monstro de fogo ou não
+
+    public GameObject[] manaBalls = new GameObject[5];
+
+    private Animator animator = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    internal void PlayHitAnim()
+    {
+        if (animator != null)
+            animator.SetTrigger("Hit");
+    }
+
+}
